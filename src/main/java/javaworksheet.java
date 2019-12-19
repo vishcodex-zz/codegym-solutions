a89835228f7f09039e228dff9cfcd5ec460f97e4
Vish Nu <vishnu.mits@gmail.com>
2019-12-19 06:51:07 -0500

    Solutions

--- src/main/java/javaworksheet.java
+++ src/main/java/javaworksheet.java
@@ -1,17 +1,44 @@
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.LinkedList;
-import java.util.Queue;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.StringWriter;
 
 public class javaworksheet {
-    public static void main(String[] args) throws URISyntaxException,StringIndexOutOfBoundsException {
-        System.out.println("### ------ This is a work sheet! --------######");
+    public static void main(String[] args) throws JAXBException
+    {
+        // Create an object to be serialized into XML
+        Cat cat = new Cat();
+        cat.name = "Missy";
+        cat.age = 5;
+        cat.weight = 4;
 
-//            URL u = new URL("https://codegym.cc/testdata/secretPasswords.txt");
-            String s = "/testdata/secretPasswords.txt";
-            String s1 = s.substring(s.lastIndexOf("/")+1,s.indexOf("."));
-            System.out.println("s1 is: " + s1);
+        // Write the result of the serialization to a StringWriter
+        StringWriter writer = new StringWriter();
+
+        // Create a Marshaller object that will perform the serialization
+        JAXBContext context = JAXBContext.newInstance(Cat.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        // And here's the serialization itself:
+        marshaller.marshal(cat, writer);
+
+        // Convert everything written to the StringWriter
+        String result = writer.toString();
+        System.out.println(result);
+    }
+
+    @XmlType(name = "cat")
+    @XmlRootElement
+    static class Cat
+    {
+        public String name;
+        public int age;
+        public int weight;
+
+        Cat()
+        {
         }
     }
+}
